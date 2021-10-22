fbe4e07ba75c9f7c2374050182cabfd45d2bcc22
FieryBlade-313 <mohdfauz.s18@iiits.in>
2021-10-22 20:59:03 +0530

    Resolved errors

--- Assets/Scripts/WeatherObject.cs
+++ Assets/Scripts/WeatherObject.cs
@@ -21,8 +21,8 @@ public class WeatherObject
             weatherName = "Blessings",
             weatherType = WeatherDefinition.WeatherType.Spring,
             weatherEffect = new WeatherEffect(2, new WeatherEffect.EffectsDeclaration[]{
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 1.2f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Gold, resourceProductionMultipler = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Gold, resourceProductionMultipler = 1.2f},
             })
         },
 
@@ -31,9 +31,9 @@ public class WeatherObject
             weatherName = "Sun's Wraith",
             weatherType = WeatherDefinition.WeatherType.Summer,
             weatherEffect = new WeatherEffect(3, new WeatherEffect.EffectsDeclaration[]{
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Stone, resourceProductionMultipler = 1.2f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Wood, resourceProductionMultipler = 0.8f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ConstructionCost, constructionCostMultiplier = 0.8f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Stone, resourceProductionMultipler = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Wood, resourceProductionMultipler = 0.8f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ConstructionCost, constructionCostMultiplier = 0.8f},
             })
         },
 
@@ -42,8 +42,8 @@ public class WeatherObject
             weatherName = "Dark Showers",
             weatherType = WeatherDefinition.WeatherType.Fall,
             weatherEffect = new WeatherEffect(2, new WeatherEffect.EffectsDeclaration[]{
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 1.2f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ConstructionCost, constructionCostMultiplier = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ConstructionCost, constructionCostMultiplier = 1.2f},
             })
         },
 
@@ -52,10 +52,10 @@ public class WeatherObject
             weatherName = "Frosty Shadows",
             weatherType = WeatherDefinition.WeatherType.Winter,
             weatherEffect = new WeatherEffect(4, new WeatherEffect.EffectsDeclaration[]{
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 0.8f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Gold, resourceProductionMultipler = 0.8f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Stone, resourceProductionMultipler = 0.8f},
-                new WeatherEffect.EffectsDeclaration(){effectType = WeatherEffect.EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Wood, resourceProductionMultipler = 1.2f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Food, resourceProductionMultipler = 0.8f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Gold, resourceProductionMultipler = 0.8f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Stone, resourceProductionMultipler = 0.8f},
+                new WeatherEffect.EffectsDeclaration(){effectType = EffectInfo.EffectType.ResourceProduction, targetResourceType = ResourceDefinition.ResourceType.Wood, resourceProductionMultipler = 1.2f},
             })
         },
     };
